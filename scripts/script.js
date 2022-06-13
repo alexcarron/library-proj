e73e009bde606b088efa0a37045b60dec7687cc5
Alex Carron <thealexcarron@gmail.com>
2022-06-13 08:43:17 -0500

    Add 'Remove Book' Function

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-let myLibrary = {};
+let myLibrary = [];
 
 
 // ! Book Constructor
@@ -11,6 +11,9 @@ function Book(title, author, total_pages, hasBeenRead) {
 
 Book.prototype.getBookInfo = function() {
 	return `The ${this.title} by ${this.author} is ${this.total_pages} pages and has ${this.hasBeenRead ? `been read` : `not been read yet`}.`
+}
+Book.prototype.toggleReadStatus = function() {
+	this.hasBeenRead = !this.hasBeenRead
 }
 
 
@@ -48,7 +51,7 @@ function addBookToLibrary() {
 		input_values.book_hasBeenRead
 	)
 
-	myLibrary[book.title] = book;
+	myLibrary.push(book);
 	
 	displayBooksInLibrary(myLibrary);
 }
@@ -60,23 +63,56 @@ function displayBooksInLibrary(library) {
 	
 	book_container.innerHTML = ''
 	
-	for (book_property in library) {
-		let book = library[book_property];
-		
-		console.log( {library, book_property, book})
+	for (book_index in library) {
+		let book = library[book_index];
 		
 		book_container.innerHTML += `
-			<div class="book">
-				<div class="top_oc_book_container">
+			<div class="book" data-book-index="${book_index}">
+				<div class="top_of_book_container">
 					<h3 class="book_title"> ${book.title} </h3>
-					<button onclick="removeBook()">
+					<button class="remove_book_button" onclick="removeBook(this)">X</button>
 				</div>
 				<p class="book_author"> by ${book.author} </p>
 				<div class="bottom_of_book_container">
 					<p class="book_total_pages"> ${book.total_pages} pages </p>
-					<p class="book_read_status ${book.hasBeenRead ? `read` : `not_read`}"> ${book.hasBeenRead ? `Read` : `Not Read`} </p>
+					<p class="book_read_status ${book.hasBeenRead ? `read` : `not_read`}" onclick="toggle_read_status(this)"> ${book.hasBeenRead ? `Read` : `Not Read`} </p>
 				</div>
 			</div>
 		`
 	}
+}
+
+
+
+function removeBook(button_element) {
+	let book_container = button_element.parentElement.parentElement;
+	let book_index = book_container.getAttribute("data-book-index");
+	
+	book_container.remove()
+	delete myLibrary[book_index];
+	
+	console.log(myLibrary);
+}
+
+
+
+function toggle_read_status(read_status_element) {
+	let book_container = read_status_element.parentElement.parentElement;
+	let book_index = book_container.getAttribute("data-book-index");
+	let book = myLibrary[book_index];
+	
+	read_status_element.classList.remove(
+		`${book.hasBeenRead ? `read` : `not_read`}`
+	)
+	
+	myLibrary[book_index].toggleReadStatus();
+	
+	read_status_element.classList.add(
+		`${book.hasBeenRead ? `read` : `not_read`}`
+	)
+	read_status_element.textContent = `
+		${book.hasBeenRead ? `Read` : `Not Read`}
+	`
+	
+	console.log(myLibrary)
 }
